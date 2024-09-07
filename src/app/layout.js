6e17ad43cd8f416740f777532f20b66e2b4a63e6
parthJuneja0 <parthjuneja040@gmail.com>
2024-09-08 00:18:22 +0530

    humming bird db

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,18 +10,20 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default async function RootLayout({ children }) {
 
-  await mongoose.connect(process.env.MONGODB_URI)
+  // await mongoose.connect(process.env.MONGODB_URI)
+  await mongoose.connect("mongodb+srv://humming:bird@cluster0.q6ccm.mongodb.net/satoshi-farm?retryWrites=true&w=majority")
+
   console.log('Connected to MongoDB');
 
   return (
     <html lang="en">
       <UserDataProvider>
         {/* <CardsProvider> */}
-          <TransactionsProvider>
-            <body className={`${inter.className} flex justify-center items-center`}>
-              {children}
-            </body>
-          </TransactionsProvider>
+        <TransactionsProvider>
+          <body className={`${inter.className} flex justify-center items-center`}>
+            {children}
+          </body>
+        </TransactionsProvider>
         {/* </CardsProvider> */}
       </UserDataProvider>
     </html>
