45b5ff783ef4de379fa701721f082c75eb308eaf
parthJuneja0 <parthjuneja040@gmail.com>
2024-09-08 14:26:59 +0530

    test mini app

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,14 +2,14 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { UserDataProvider } from "@/context/userDataContext";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import { TransactionsProvider } from "@/context/transactionsContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default async function RootLayout({ children }) {
 
-  await mongoose.connect(process.env.MONGODB_URI)
+  // await mongoose.connect(process.env.MONGODB_URI)
   // await mongoose.connect("mongodb+srv://humming:bird@cluster0.q6ccm.mongodb.net/satoshi-farm?retryWrites=true&w=majority")
 
   console.log('Connected to MongoDB');
