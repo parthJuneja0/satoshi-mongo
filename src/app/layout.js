737aa929f8b5d7fa5500f720cd88a48a1bf2ac08
parthJuneja0 <parthjuneja040@gmail.com>
2024-09-08 11:03:59 +0530

    test onClick

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { UserDataProvider } from "@/context/userDataContext";
-import mongoose from "mongoose";
-// import { CardsProvider } from "@/context/cardsContext";
+// import mongoose from "mongoose";
 import { TransactionsProvider } from "@/context/transactionsContext";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,20 +10,18 @@ const inter = Inter({ subsets: ["latin"] });
 export default async function RootLayout({ children }) {
 
   // await mongoose.connect(process.env.MONGODB_URI)
-  await mongoose.connect("mongodb+srv://humming:bird@cluster0.q6ccm.mongodb.net/satoshi-farm?retryWrites=true&w=majority")
+  // await mongoose.connect("mongodb+srv://humming:bird@cluster0.q6ccm.mongodb.net/satoshi-farm?retryWrites=true&w=majority")
 
   console.log('Connected to MongoDB');
 
   return (
     <html lang="en">
       <UserDataProvider>
-        {/* <CardsProvider> */}
         <TransactionsProvider>
           <body className={`${inter.className} flex justify-center items-center`}>
             {children}
           </body>
         </TransactionsProvider>
-        {/* </CardsProvider> */}
       </UserDataProvider>
     </html>
   );
