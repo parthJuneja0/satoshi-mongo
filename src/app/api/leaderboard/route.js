4555379bb2a5429eea699a59fa8b68cc2939de4a
jainharsh2106 <100188988+jainharsh2106@users.noreply.github.com>
2024-09-24 22:23:35 +0530

    Update route.js

--- src/app/api/leaderboard/route.js
+++ src/app/api/leaderboard/route.js
@@ -8,7 +8,7 @@ export async function GET() {
     try {
         const topPlayers = await User.find({})
             .sort({ yieldPerHour: -1 }) // Sort by yieldPerHour in descending order
-            .limit(100) // Limit to top 100 players
+            .limit(1000) // Limit to top 100 players
             .select("username yieldPerHour telegramId"); // Select fields you need
 
         return NextResponse.json({ topPlayers });
